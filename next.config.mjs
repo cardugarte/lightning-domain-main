f246eecafe0ced4135ea7c7cd0c290cb7e197af7
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-08-25 08:59:57 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -36,7 +36,6 @@ const config = {
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-          { key: 'Access-Control-Allow-Origin', value: 'https://xplorers.ar' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET, DELETE, PATCH, POST, PUT, OPTIONS',
